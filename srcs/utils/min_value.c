65c9c7b8507a93f4c5055548cc2ce51ba044b6ff
xenia <xeniavislocka@gmail.com>
2024-04-10 17:21:13 +0200

    move helper functions to utils folder

--- srcs/utils/min_value.c
+++ srcs/utils/min_value.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   min_value.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vconesa- <vconesa-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: xvislock <xvislock@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 12:05:42 by xvislock          #+#    #+#             */
-/*   Updated: 2024/03/20 16:22:46 by vconesa-         ###   ########.fr       */
+/*   Updated: 2024/04/10 17:20:20 by xvislock         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/ft_map.h"
+#include "../../includes/ft_map.h"
 
 int	min_value(int a, int b, int c)
 {
